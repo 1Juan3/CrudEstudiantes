bc03f0c2fe946970082e1ef768a9ddd7b9b0d0a9
Juan Gabriel <jgalfonso9@misena.edu.co>
2022-10-11 20:41:37 -0500

    este es el crud de estudiantes con firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyC4evZVgJK000R5JhdpM9MQIhnK603eNO8",
+    authDomain: "veterinaria-e739d.firebaseapp.com",
+    databaseURL: "https://veterinaria-e739d-default-rtdb.firebaseio.com",
+    projectId: "veterinaria-e739d",
+    storageBucket: "veterinaria-e739d.appspot.com",
+    messagingSenderId: "260943399789",
+    appId: "1:260943399789:web:e12826532d7e2ab8db6864"
+  }
+};
